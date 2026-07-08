8ee93d38b1ac6e28df9db15b6d07763b99034ef6
Amanmishra30 <amanmishra6962@gmail.com>
2026-07-08 23:45:13 +0530

    Recipes

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+
 
 const Create = () => {
+  const {register, handleSubmit} = useForm()
   return (
-    <div>
-      
-    </div>
+    <form >
+      <input
+      className=" block border-b outline-0 p-2"
+       {...register("image")} 
+       type = "file" 
+       />
+       <small className = "text-red-500"> 
+       This is how the error is shown 
+       </small>
+       <textarea
+      className="border-b outline-0 p-2"
+       {...register("enter from here")} 
+       placeholder="Recipes Title">
+        </textarea>
+
+       <small className = "text-red-500"> 
+       This is how the error is shown 
+       </small> 
+       <textarea
+      className="border-b outline-0 p-2"
+       {...register("enter from here")} 
+       placeholder="Recipes Title">
+        </textarea>
+
+       <small className = "text-red-500"> 
+       This is how the error is shown 
+       </small> 
+    </form>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,20 @@ import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div classname =" flex justify-center gap-x-10 text-sm mb=1">
+    <div classname =" flex  item-center  justify-center gap-x-10 text-sm mb=1">
       
-      <NavLink className= {(e) => e.isActive && "text-red-400"} to="/">
+      <NavLink className= {(e) => e.isActive ?  "text-red-400" : " "} to="/">
        Home
       </NavLink>
-        <NavLink className= {(e) => e.isActive && "text-red-400"} to="/Recipes">
+        <NavLink className= {(e) => e.isActive ?  "text-red-400" : " "} to="/Recipes">
        Recipes
       </NavLink>
-        <NavLink className= {(e) => e.isActive && "text-red-400"} to="/About">
+        <NavLink className= {(e) => e.isActive ?"text-red-400" : " "} to="/about">
        About
+      </NavLink>
+       <NavLink className= {` px-4 py-2 bg-gray-900 rounded $(e) => e.isActive ? "text-red-400" : " "`} 
+       to="/Create-Reacipe">
+       Create Recipes
       </NavLink>
      
     </div>
